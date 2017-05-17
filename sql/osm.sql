965711faf7d830c21f824f982d13accc3d9a537f
LiuPing <465638135@qq.com>
2017-05-17 10:41:52 +0800

    修改sql格式

--- sql/osm.sql
+++ sql/osm.sql
@@ -5,7 +5,7 @@ create table role(
 	mark  varchar2(200),
 	status  number(2)
 );
-create sequenceseq_role start with 10001;
+create sequence seq_role start with 10001;
 
 --管理员信息表
 create table adminInfo(
@@ -18,7 +18,7 @@ create table adminInfo(
 	photo varchar2(400),
 	status number(2)--状态：0待审核1审核未通过2已审核,可以正常登录3冻结
 );
-create sequenceseq_adminInfo start with 10001;
+create sequence seq_adminInfo start with 10001;
 
 --用户信息表
 create table userInfo(
@@ -36,7 +36,7 @@ create table userInfo(
 	status number(2),--会员状态：0会员1：普通用户
 	des varchar2(200)--说明
 );
-create sequenceseq_userInfo start with 10001;
+create sequence seq_userInfo start with 10001;
 
 --商品类型
 create table type(
@@ -45,7 +45,7 @@ create table type(
 	des varchar2(200),--类型描述
 	status number(2)--类型的状态（1、上架1、下架）
 );
-create sequenceseq_type start with 10001;
+create sequence seq_type start with 10001;
 
 --商店信息
 create table store(
@@ -59,7 +59,7 @@ create table store(
 	status number(2),--状态(0:待审核1:审核未通过2:通过3:释放(店铺已被关闭))
 	info clob
 );
-create sequenceseq_store start with 10001;
+create sequence seq_store start with 10001;
 
 --商品表
 create table commodity(
@@ -70,7 +70,7 @@ create table commodity(
 	picture varchar2(2000),--照片
 	status number(2)--状态(上架，下架)
 );
-create sequenceseq_commodity start with 10001;
+create sequence seq_commodity start with 10001;
 
 --商品类型表
 create table commodity_type(
@@ -78,7 +78,7 @@ create table commodity_type(
 	tid number(10),--类型id
 	cid number(10)--商品id
 );
-create sequenceseq_commodity_type start with 10001;
+create sequence seq_commodity_type start with 10001;
 
 --活动表
 create table activity(
@@ -88,9 +88,9 @@ create table activity(
 	startdate date,
 	enddate date,
 	personnum number(2),--几人餐
-	contentclob--活动介绍
+	content clob--活动介绍
 );
-create sequenceseq_activity start with 10001;
+create sequence seq_activity start with 10001;
 
 --商品活动联系表
 create table commodity_activity(
@@ -98,7 +98,7 @@ create table commodity_activity(
 	cid number(10),--商品id
 	aid number(10)--活动id
 );
-create sequenceseq_commodity_activity start with 10001;
+create sequence seq_commodity_activity start with 10001;
 
 --订单表
 create table orders(
@@ -108,7 +108,7 @@ create table orders(
 	status number(2),--订单状态
 	odate date--订单日期
 );
-create sequenceseq_order start with 10001;
+create sequence seq_order start with 10001;
 
 --商店订单联系表
 create table store_order(
@@ -116,7 +116,7 @@ create table store_order(
 	oid varchar2(200),
 	sid number(10)
 );
-create sequenceseq_store_order start with 10001;
+create sequence seq_store_order start with 10001;
 
 --消息表
 create table message(
@@ -125,4 +125,4 @@ create table message(
 	content varchar2(4000),
 	mdate date
 );
-create sequenceseq_message start with 10001;
+create sequence seq_message start with 10001;
